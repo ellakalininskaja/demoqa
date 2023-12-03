fa09241cdc99b9c15db4fda57a5930c2d194a200
ellakalininskaja <ellakalininskaja@yandex.ru>
2023-12-03 23:05:39 +0300

    hw9

---
+++ tests/test_page_dialogs.py
@@ -0,0 +1,31 @@
+from conftest import browser
+from pages.demoqa import DemoQa
+from pages.modal_dialogs import Modal_dialogs
+
+const_main_page_title = 'DEMOQA'
+
+
+def test_find_modal_elements(browser):
+    modal = Modal_dialogs(browser)
+    modal.visit()
+
+    assert modal.accordion_menu_alerts_btns.check_count_elements(countItems=5)
+
+
+def test_navigation_modal(browser):
+    modal = Modal_dialogs(browser)
+    demo_qa_page: DemoQa = DemoQa(browser)
+
+    modal.visit()
+    modal.refresh()
+
+    browser.refresh()
+
+    modal.main_page_icon.click()
+
+    browser.back()
+    browser.set_window_size(900, 400)
+    browser.forward()
+
+    assert demo_qa_page.equal_url()
+    assert browser.title == const_main_page_title
