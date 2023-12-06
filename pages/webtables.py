42fe9d68508f6b30d31c502a9dd59979bfd8526e
ellakalininskaja <ellakalininskaja@yandex.ru>
2023-12-07 01:35:29 +0300

    hw11

--- pages/webtables.py
+++ pages/webtables.py
@@ -11,4 +11,16 @@ class Webtables(BasePage):
 
         self.no_data = WebElement(driver, "div.rt-noData")
         self.btn_delete_row = WebElement(driver, "//*[contains(@title, 'Delete')]", "xpath")
-
+        self.add_btn = WebElement(driver, "#addNewRecordButton")
+        self.first_name = WebElement(driver, "#firstName")
+        self.last_name = WebElement(driver, "#lastName")
+        self.user_email = WebElement(driver, "#userEmail")
+        self.age = WebElement(driver, "#age")
+        self.salary = WebElement(driver, "#salary")
+        self.department = WebElement(driver, "#department")
+        self.submit_btn = WebElement(driver, "#submit")
+        self.btn_edit_row = WebElement(driver, "//*[contains(@title, 'Edit')]", "xpath")
+        self.table_row_first_name = WebElement(driver, "#app > div > div > div.row > div.col-12.mt-4.col-md-6 > "
+                                                       "div.web-tables-wrapper > div.ReactTable.-striped.-highlight > "
+                                                       "div.rt-table > div.rt-tbody > div:nth-child(1) > div > "
+                                                       "div:nth-child(1)")
--- tests/test_webtables.py
+++ tests/test_webtables.py
@@ -3,6 +3,8 @@ import time
 from conftest import browser
 from pages.webtables import Webtables
 
+const_first_name_text = 'tester222222'
+
 
 def test_webtables(browser):
     page_tables = Webtables(browser)
@@ -13,5 +15,21 @@ def test_webtables(browser):
     while page_tables.btn_delete_row.exist():
         page_tables.btn_delete_row.click()
 
-    time.sleep(3)
     assert page_tables.no_data.exist()
+
+    page_tables.add_btn.click()
+    page_tables.first_name.send_keys('tester')
+    page_tables.last_name.send_keys('testerovich')
+    page_tables.user_email.send_keys('test@ttt.tt')
+    page_tables.age.send_keys('30')
+    page_tables.salary.send_keys('1111')
+    page_tables.department.send_keys('department2222')
+    page_tables.submit_btn.click()
+    assert page_tables.btn_edit_row.exist()
+    page_tables.btn_edit_row.click()
+    page_tables.first_name.clear()
+    page_tables.first_name.send_keys(const_first_name_text)
+    page_tables.submit_btn.click()
+    assert page_tables.table_row_first_name.get_text() == const_first_name_text
+    page_tables.btn_delete_row.click()
+    assert not page_tables.btn_delete_row.exist()
